70c067ae1210e8c5d7139ddffcdc332e884598c2
Frederic Morais-Simard <fmsimard@cae.com>
2024-10-09 13:40:08 -0400

    call google

--- src/main/java/ch/hsr/cloud/docker/helloworld/Application.java
+++ src/main/java/ch/hsr/cloud/docker/helloworld/Application.java
@@ -2,20 +2,30 @@ package ch.hsr.cloud.docker.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class Application {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker Cloud World!";
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker Cloud World!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Scheduled(fixedRate = 30000)
+    public void sendGetRequest() {
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject("https://www.google.com", String.class);
+        System.out.println("Response from Google: " + response);
+    }
 }
